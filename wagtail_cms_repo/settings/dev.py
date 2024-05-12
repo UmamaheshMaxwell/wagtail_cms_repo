676a2c5a264e1e8909a1ada7f1852eb45621a274
Umamaheswararao Meka <maheshmeka16@gmail.com>
2024-05-12 22:20:52 +0530

    basic setup

---
+++ wagtail_cms_repo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@%mr3l##5&l9k-l(b+!!clj7lxy@2n!29=izd&(u4oy2&m9b=e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
